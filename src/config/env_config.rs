2a745025d12a6a7b5e458e911ba13bb6f5781b9c
Sakuya200 <q1297206879@gmail.com>
2026-05-09 21:57:42 +0800

    配置解析代码完善

--- src/config/env_config.rs
+++ src/config/env_config.rs
@@ -1,7 +1,7 @@
-use std::path::Path;
-use serde::{Deserialize, Serialize};
-use anyhow::Result;
 use crate::pipeline::{AttentionImplementation, HardwareType};
+use serde::{Deserialize, Serialize};
+use std::env::current_dir;
+use std::path::PathBuf;
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq, Default)]
 #[serde(rename_all = "snake_case")]
@@ -14,12 +14,12 @@ pub struct EnvConfig {
     pub training: TrainingConfig,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq, Default)]
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
 #[serde(rename_all = "snake_case", default)]
 pub struct BasicConfig {
-    pub data_dir: Option<String>,
-    pub log_dir: Option<String>,
-    pub model_dir: Option<String>,
+    pub data_dir: String,
+    pub log_dir: String,
+    pub model_dir: String,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
@@ -35,6 +35,16 @@ pub struct TrainingConfig {
     pub attn_implementation: AttentionImplementation,
 }
 
+impl Default for BasicConfig {
+    fn default() -> Self {
+        Self {
+            data_dir: "./data".to_string(),
+            log_dir: "./logs".to_string(),
+            model_dir: "./models".to_string(),
+        }
+    }
+}
+
 impl Default for TrainingConfig {
     fn default() -> Self {
         Self {
@@ -46,9 +56,7 @@ impl Default for TrainingConfig {
 
 impl Default for ServerConfig {
     fn default() -> Self {
-        Self {
-            port: 3000,
-        }
+        Self { port: 3000 }
     }
 }
 
@@ -67,20 +75,47 @@ impl TrainingConfig {
     }
 }
 
-
 impl EnvConfig {
-
     /// 将配置写回到配置文件中
-    pub fn flush_env_config(&self) -> Result<()> {
-        Ok(())
+    pub fn flush_env_config(&self) {
+        let toml_path = current_dir()
+            .expect("failed to get current directory")
+            .join("config.toml");
+        let toml_str = toml::to_string_pretty(self)
+            .expect("Failed to serialize EnvConfig to TOML string");
+        std::fs::write(&toml_path, toml_str)
+            .unwrap_or_else(|e| panic!("Failed to write config file at {:?}: {e}", toml_path));
     }
 
-    pub fn from_config_toml(toml_path: &Path) -> EnvConfig {
-        let toml_str = std::fs::read_to_string(toml_path).unwrap_or_else(|e| {
-            panic!("Failed to read config file at {:?}: {e}", toml_path)
-        });
-        toml::from_str(&toml_str).unwrap_or_else(|e| {
-            panic!("Failed to parse config file at {:?}: {e}", toml_path)
-        })
+    pub fn from_config_toml() -> EnvConfig {
+        let toml_path = current_dir()
+            .expect("failed to get current directory")
+            .join("config.toml");
+        if !toml_path.exists() {
+            println!("config file does not exist: {:?}, app will create a new config file", toml_path);
+            // 如果文件不存在则创建新文件
+            std::fs::File::create(&toml_path)
+                .expect("failed to create config.toml");
+        }
+        let toml_str = std::fs::read_to_string(&toml_path)
+            .unwrap_or_else(|e| panic!("Failed to read config file at {:?}: {e}", toml_path));
+        toml::from_str(&toml_str)
+            .unwrap_or_else(|e| panic!("Failed to parse config file at {:?}: {e}", toml_path))
+    }
+
+    pub fn log_dir(&self) -> PathBuf {
+        self.basic.log_dir.clone().into()
+    }
+
+    pub fn model_dir(&self) -> PathBuf {
+        self.basic.model_dir.clone().into()
+    }
+
+    pub fn hardware_type(&self) -> HardwareType {
+        self.training.hardware_type.clone()
+    }
+
+    pub fn attn_implementation(&self) -> AttentionImplementation {
+        self.training.attn_implementation.clone()
     }
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,10 +2,11 @@ mod env_config;
 mod log_config;
 mod ui_config;
 
-use std::env::current_dir;
 pub use env_config::EnvConfig;
 
 pub fn init_config() -> EnvConfig {
-    let cur_dir = current_dir().expect("failed to get current directory");
-    EnvConfig::from_config_toml(cur_dir.as_path())
+    let env_config = EnvConfig::from_config_toml();
+    // 将默认值写回配置文件
+    env_config.flush_env_config();
+    env_config
 }
