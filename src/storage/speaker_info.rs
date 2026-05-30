05039dceb438d63216c0f6caab680989286036dc
Sakuya200 <q1297206879@163.com>
2026-05-30 18:30:32 +0800

    speaker相关接口实现框架代码

--- src/storage/speaker_info.rs
+++ src/storage/speaker_info.rs
@@ -3,48 +3,49 @@ use crate::models::speaker::{
 };
 use crate::storage::LocalStorage;
 use async_trait::async_trait;
+use anyhow::Result;
 
 #[async_trait]
 pub trait SpeakerInfoStorage {
     async fn create_speaker_info(
         &self,
         request: CreateSpeakerRequest,
-    ) -> anyhow::Result<SpeakerInfoResponse>;
+    ) -> Result<SpeakerInfoResponse>;
 
     async fn import_model_as_speaker(
         &self,
         request: ImportModelAsSpeakerRequest,
-    ) -> anyhow::Result<SpeakerInfoResponse>;
+    ) -> Result<SpeakerInfoResponse>;
 
-    async fn list_speaker_infos(&self) -> anyhow::Result<Vec<SpeakerInfoResponse>>;
+    async fn list_speaker_infos(&self) -> Result<Vec<SpeakerInfoResponse>>;
 
     async fn update_speaker_info(
         &self,
         request: UpdateSpeakerRequest,
     ) -> anyhow::Result<SpeakerInfoResponse>;
 
-    async fn delete_speaker_info(&self, speaker_id: i64) -> anyhow::Result<bool>;
+    async fn delete_speaker_info(&self, speaker_id: i64) -> Result<bool>;
 }
 
 #[async_trait]
 impl SpeakerInfoStorage for LocalStorage {
-    async fn create_speaker_info(&self, request: CreateSpeakerRequest) -> anyhow::Result<SpeakerInfoResponse> {
+    async fn create_speaker_info(&self, request: CreateSpeakerRequest) -> Result<SpeakerInfoResponse> {
         todo!()
     }
 
-    async fn import_model_as_speaker(&self, request: ImportModelAsSpeakerRequest) -> anyhow::Result<SpeakerInfoResponse> {
+    async fn import_model_as_speaker(&self, request: ImportModelAsSpeakerRequest) -> Result<SpeakerInfoResponse> {
         todo!()
     }
 
-    async fn list_speaker_infos(&self) -> anyhow::Result<Vec<SpeakerInfoResponse>> {
+    async fn list_speaker_infos(&self) -> Result<Vec<SpeakerInfoResponse>> {
         todo!()
     }
 
-    async fn update_speaker_info(&self, request: UpdateSpeakerRequest) -> anyhow::Result<SpeakerInfoResponse> {
+    async fn update_speaker_info(&self, request: UpdateSpeakerRequest) -> Result<SpeakerInfoResponse> {
         todo!()
     }
 
-    async fn delete_speaker_info(&self, speaker_id: i64) -> anyhow::Result<bool> {
+    async fn delete_speaker_info(&self, speaker_id: i64) -> Result<bool> {
         todo!()
     }
 }
